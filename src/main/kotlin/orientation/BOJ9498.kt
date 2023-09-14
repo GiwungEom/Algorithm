53d77ed1049727f778e3c92edcf8fd2cc22c049f
Giwung <kiwung0701@gmail.com>
2023-09-14 15:12:00 +0900

    BOJ 9498 문제 풀이 완료

---
+++ src/main/kotlin/orientation/BOJ9498.kt
@@ -0,0 +1,39 @@
+package orientation
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+/**
+ * 문제
+ * 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+ *
+ * 입력
+ * 첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+ *
+ * 출력
+ * 시험 성적을 출력한다.
+ *
+ * 예제 입력 1
+ * 100
+ * 예제 출력 1
+ * A
+ */
+
+fun main(arg: Array<String>) {
+    val bufferedReader = BufferedReader(InputStreamReader(System.`in`))
+    val bufferedWriter = BufferedWriter(OutputStreamWriter(System.out))
+
+    bufferedWriter.append(
+        when (bufferedReader.readLine().toInt()) {
+            in 90..100 -> "A"
+            in 80..89 -> "B"
+            in 70..79 -> "C"
+            in 60..69 -> "D"
+            else -> "F"
+        }
+    ).flush()
+    bufferedWriter.close()
+    bufferedReader.close()
+}
