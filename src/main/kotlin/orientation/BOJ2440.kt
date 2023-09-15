db44d89519c30c1d0955522414bc9781f3bc4ea3
Giwung <kiwung0701@gmail.com>
2023-09-15 16:06:56 +0900

    BOJ 2440 문제 풀이 완료

---
+++ src/main/kotlin/orientation/BOJ2440.kt
@@ -0,0 +1,43 @@
+package orientation
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+/**
+ * 문제
+ * 첫째 줄에는 별 N개, 둘째 줄에는 별 N-1개, ..., N번째 줄에는 별 1개를 찍는 문제
+ *
+ * 입력
+ * 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+ *
+ * 출력
+ * 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+ *
+ * 예제 입력 1
+ * 5
+ * 예제 출력 1
+ * *****
+ * ****
+ * ***
+ * **
+ * *
+ */
+
+fun main() {
+    val bufferedReader = BufferedReader(InputStreamReader(System.`in`))
+    val bufferedWriter = BufferedWriter(OutputStreamWriter(System.out))
+
+    val n = bufferedReader.readLine().toInt()
+    for (i in n downTo  1) {
+        for (j in 1..i) {
+            bufferedWriter.append("*")
+        }
+        bufferedWriter.appendLine()
+    }
+    bufferedWriter.flush()
+    bufferedWriter.close()
+    bufferedReader.close()
+}
